166103a8e89c44d253018ed07239ac8c50366fe9
Javier Desktop <javierixcolin202132163@cunoc.edu.gt>
2023-08-16 00:32:32 -0600

    Creacion de clase

---
+++ analizador_lexico/src/main/java/com/mycompany/analizador_lexico/Analizador_lexico.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.analizador_lexico;
+
+import com.mycompany.analizador_lexico.gui.mainJFrame;
+
+/**
+ *
+ * @author hamme
+ */
+public class Analizador_lexico {
+
+    public static void main(String[] args) {
+        mainJFrame mainWindow = new mainJFrame();
+        mainWindow.setVisible(true);
+    }
+}
